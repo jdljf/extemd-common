c1d8df264854275fb1d3d8bb0f734efcd8fa1eb5
jdljf <1243270526@qq.com>
2019-05-30 11:36:53 +0800

    功能：添加为一个DOM添加或删除一个类的方法

--- common.js
+++ common.js
@@ -1,3 +1,4 @@
+// 数组 async some
 Array.prototype.someSync = async function (callback, thisArr) {
 	for (let [index, item] of Object.entries(this)) {
 		if (await callback(item, index, this)) return true
@@ -5,9 +6,35 @@ Array.prototype.someSync = async function (callback, thisArr) {
 	return false
 }
 
+// 数组 async every
 Array.prototype.everySync = async function (callback, thisArr) {
 	for (const [index, item] of Object.entries(this)) {
 		if (!await callback(item, index, this)) return false
 	}
 	return true
+}
+
+// 字符串去掉两边的空格
+function strTrim(str){
+	return str.replace(/(^\s*)|(\s*$)/g, '')
+}
+
+// 判断某个 DOM 是否有某个类（class）
+function hasClass (elementDom, className) {
+	// ( \\s|^ ) 判断前面是否有空格 （\\s|$ ）判断后面是否有空格 两个感叹号为转换为布尔值 以方便做判断
+	return !!elementDom.className.match(new RegExp("(\\s|^)" + className + "(\\s|$)"));
+}
+
+// 给元素添加class，首先使用hasClass方法进行了判断
+function addClass(elementDom, className) {
+	if (!hasClass(elementDom, className)) {
+		elementDom.className += ' ' + className
+	}
+}
+
+// 删除元素的一个class
+function deleteClass(elementDom, className) {
+	if (hasClass(elementDom, className)) {
+		elementDom.className = elementDom.className.replace(new RegExp("(\\s|^)" + className + '(\\s|$)'), '')
+	}
 }
